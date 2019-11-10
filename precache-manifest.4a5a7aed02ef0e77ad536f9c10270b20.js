3ae0741afb433620b4a2b81a4e1539e372a5fcf6
bofa <prosit86@gmail.com>
2019-11-10 16:51:02 +0100

    new build

---
+++ precache-manifest.4a5a7aed02ef0e77ad536f9c10270b20.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e144934e6450062490184c8b0dba39d9",
+    "url": "/stocks/index.html"
+  },
+  {
+    "revision": "d749126f5941baff5c70",
+    "url": "/stocks/static/css/2.623fcd91.chunk.css"
+  },
+  {
+    "revision": "0ed7121c6f2438d085b2",
+    "url": "/stocks/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "d749126f5941baff5c70",
+    "url": "/stocks/static/js/2.a4492115.chunk.js"
+  },
+  {
+    "revision": "0ed7121c6f2438d085b2",
+    "url": "/stocks/static/js/main.89ad5404.chunk.js"
+  },
+  {
+    "revision": "34586ebeb26d40bfea31",
+    "url": "/stocks/static/js/runtime-main.4557de29.js"
+  },
+  {
+    "revision": "b62ce8316edcf6cd91b261e1b6db07ea",
+    "url": "/stocks/static/media/icons-16.b62ce831.eot"
+  },
+  {
+    "revision": "e0aef47b375af9a5e8a601ea48e74a45",
+    "url": "/stocks/static/media/icons-16.e0aef47b.ttf"
+  },
+  {
+    "revision": "e2458cd59cf9786664a5fb610d6f19f1",
+    "url": "/stocks/static/media/icons-16.e2458cd5.woff"
+  },
+  {
+    "revision": "748778e0d622390feff65d4086a7cf13",
+    "url": "/stocks/static/media/icons-20.748778e0.ttf"
+  },
+  {
+    "revision": "95f6660eb5ef987149ab2e79dfe47046",
+    "url": "/stocks/static/media/icons-20.95f6660e.eot"
+  },
+  {
+    "revision": "9f1e61641e0a1d3f70d4bbaa66f5122c",
+    "url": "/stocks/static/media/icons-20.9f1e6164.woff"
+  }
+]);
